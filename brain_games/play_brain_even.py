7605b71629583fa2aa87039cd38dabfbf9354b5c
DS85 <dimon85@gmail.com>
2022-05-10 00:33:57 +0400

    brain-even

---
+++ brain_games/play_brain_even.py
@@ -0,0 +1,41 @@
+import random
+import prompt
+
+
+def play_brain_even():
+    n = random.randrange(100)
+    print('Question: ', n)
+    answer = prompt.string('Your answer: ')
+    answer.lower()
+    
+    # Определеяем правильный ответ
+    correct_answer = ''
+    if n % 2 == 0:
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+    
+    # Если ответ правильный:
+    global yes_count
+    if answer == correct_answer:
+        print('Correct!')
+        yes_count += 1
+        if yes_count == 3:
+            print(f'Contgratulations, {name}!')
+            play_brain_even()
+        else:
+            play_brain_even()
+    
+    # Если ответ неправильный:
+    else:
+        print(f"'{answer}', is wrong answer ;(. Correct answer was '{correct_answer}'")
+        print(f"Let's try again, {name}!")
+        yes_count = 0
+        play_brain_even()
+
+
+name = prompt.string('May I have your name? ')
+print(f'Hello, {name}')
+print('Answer "yes" if the number is even, otherwise answer "no".')
+yes_count = 0
+play_brain_even()
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+from brain_games.play_brain_even import play_brain_even
+
+
+def main():
+    print('Welcome to the Brain Games!')
+
+
+play_brain_even()
+
+if __name__ == '__main__':
+    main()
