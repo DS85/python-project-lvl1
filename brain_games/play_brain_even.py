3475233a02926f7acab454d2036a8c23c432e080
DS85 <dimon85@gmail.com>
2022-05-10 20:45:14 +0400

    step5

--- brain_games/play_brain_even.py
+++ brain_games/play_brain_even.py
@@ -7,14 +7,14 @@ def play_brain_even():
     print('Question: ', n)
     answer = prompt.string('Your answer: ')
     answer.lower()
-    
+
     # Определеяем правильный ответ
     correct_answer = ''
     if n % 2 == 0:
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
-    
+
     # Если ответ правильный:
     global yes_count
     if answer == correct_answer:
@@ -22,10 +22,11 @@ def play_brain_even():
         yes_count += 1
         if yes_count == 3:
             print(f'Contgratulations, {name}!')
+            yes_count = 0
             play_brain_even()
         else:
             play_brain_even()
-    
+
     # Если ответ неправильный:
     else:
         print(f"'{answer}', is wrong answer ;(. Correct answer was '{correct_answer}'")
