95c4e3c954993b2ea43c46a9642efe5ec9ee4c64
DS85 <dimon85@gmail.com>
2022-05-11 16:07:37 +0400

    step-8 brain-progression

---
+++ brain_games/games/play_brain_progression.py
@@ -0,0 +1,31 @@
+import prompt
+import random
+from brain_games.games.game_logics import greeting, check_answer
+
+
+def play_brain_progression():
+    # Приветствие и начало игры
+    greeting('What number is missing in the progression?')
+    rounds_count = 0
+    yes_count = 0
+
+    while rounds_count < 3:
+        rounds_count += 1
+
+        # Генерируем случайные значения для вопроса
+        start = random.randint(0, 1)
+        step = random.randint(2, 5)
+        stop = start + step * 10
+        hidden_i = random.randint(0, 9)
+        seq = list(range(start, stop, step))
+
+        # Определеяем правильный ответ
+        correct_answer = seq[hidden_i]
+        seq[hidden_i] = '...'
+
+        # Задаем вопрос и получаем ответ
+        print('Question: ', *seq)
+        answer = prompt.string('Your answer: ')
+
+        # Обрабатываем ответ
+        yes_count = check_answer(answer, correct_answer, yes_count)
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from brain_games.games.play_brain_progression import play_brain_progression
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    play_brain_progression()
+
+
+if __name__ == '__main__':
+    main()
